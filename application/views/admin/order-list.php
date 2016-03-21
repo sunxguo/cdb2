56f23a64e45f97a491072641754a4d3338c5a71f
MaTengfei <1042930803@qq.com>
2016-03-21 19:37:27 +0800

    update

--- application/views/admin/order-list.php
+++ application/views/admin/order-list.php
@@ -72,13 +72,7 @@
 				<td><u style="cursor:pointer" class="text-primary" onclick="member_show('<?php echo $order->orderno;?>','/admin/ordershow','<?php echo $order->orderno;?>','360','440')"><?php echo $order->orderno;?></u></td>
 				<td>
 
-				<?php foreach($order->details as $detail):?>
-						<?php if(!empty($detail->product->name)):?>
-							<img src="<?php echo $detail->product->pic1;?>" title="<?php echo $detail->product->name;?>" alt="<?php echo $detail->product->name;?>" width="30"> × <?php echo $detail->count;?>
-						<?php else:?>
-						    商品不存在
-						<?php endif;?>	
-					<?php endforeach;?>
+				
 				    
 					<?php foreach($order->details as $detail):?>
 						
